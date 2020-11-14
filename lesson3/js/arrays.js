3f72d84763d3421b0c6679872aa4d88f7fef271b
nikita <adrbrug@gmail.com>
2020-11-14 12:09:29 +0300

    ++

---
+++ lesson3/js/arrays.js
@@ -0,0 +1,10 @@
+'use strict';
+// пустой массив
+let arr = [];
+console.log(arr);
+
+// массив с элементами
+let colors = ["red", "blue", "yellow", "green"];
+
+// доступ к элементам массива осуществляется по его имени через []
+console.log(colors);
