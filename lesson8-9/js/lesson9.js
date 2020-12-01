d12357ad8fdcd313423b690dfa866ccb94d44415
nikita <adrbrug@gmail.com>
2020-12-01 22:30:51 +0300

    lesson9++

--- lesson8-9/js/lesson9.js
+++ lesson8-9/js/lesson9.js
@@ -57,11 +57,49 @@ let userInfo = {avatar: "img/avatar.jpg", name: "Ирина"};
 addComment("wq eqr tt 5ywy", userInfo,
     document.querySelector(".comments"));
 
+console.log(document.getElementById("game-field"));
 
 // функция generateField(n) генерации игрового поля из дз #8
+function generateField(n) {
+    let container = document.createElement("div");
+    container.style.width = '40vw';
+    container.style.display = 'flex';
+    container.style.flexWrap = 'wrap';
+    let size = 40/n; //'vw'
+    for (let i = 0; i < n * n; i++) {
+        let innerDiv = document.createElement("div");
+        innerDiv.style.border = '1px solid black';
+        innerDiv.style.width = innerDiv.style.height = size + 'vw';
+        container.append(innerDiv);
+    }
+    document.getElementById("game-field").append(container);
+}
+generateField(5);
 
 // карусель
+let slider = {
+    init: function (id) { // метод инициализация
+        // объекту slider добавили свойства slides и currentSlide
+        // и присвоили им значения
+        this.slides = document.getElementById(id).children;
+        this.currentSlide = 0; // индекс текущего слайда
+    },
+    nextSlide: function () { // логика переключения слайдов
+        this.slides[this.currentSlide].classList.remove("active");
+        this.currentSlide += 1;
+        this.currentSlide = this.currentSlide < this.slides.length ?
+            this.currentSlide : 0;
+        this.slides[this.currentSlide].classList.add("active");
+    },
+    run: function () { // метод запуск nextSlide с интервалом
+        // this.nextSlide(); // this - контекст вызова функции
+        setInterval(this.nextSlide.bind(this), 4000); // 4000 млс
+    }
+};
+slider.init("slider"); // вызов метода init объекта slider
+slider.run(); // вызов метода run объекта slider
 
+// slider.slides = document.getElementById('id').children;
+// slider.currentSlide = 0;
 
-
-
+// Array.from(htmlCollection).forEach(htmlElem => );
